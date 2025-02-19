1ddf42a72f6bd66dba32dfbd1d2c47f6287968b5
Ronan <ronan.dupas@ynov.com>
2025-02-19 14:49:01 +0100

    récupération de la base de données

--- Projet_Ymmersion/config/bundles.php
+++ Projet_Ymmersion/config/bundles.php
@@ -10,7 +10,4 @@ return [
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
-
-
-
 ];
