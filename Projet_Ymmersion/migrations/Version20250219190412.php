3691f42f755f89d7afb8e2c7f49dcb04110dc6ea
EdgarCou <edgar.couthouis@ynov.com>
2025-02-20 08:19:19 +0100

    make group and join them, habit for group

--- Projet_Ymmersion/migrations/Version20250219190412.php
+++ Projet_Ymmersion/migrations/Version20250219190412.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250218133051 extends AbstractMigration
+final class Version20250219190412 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20250218133051 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE habit (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, text VARCHAR(255) NOT NULL, difficulty VARCHAR(50) NOT NULL, color VARCHAR(7) NOT NULL, periodicity VARCHAR(20) NOT NULL, created_at DATETIME NOT NULL)');
+        $this->addSql('ALTER TABLE habit ADD type VARCHAR(10) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE habit');
+        $this->addSql('ALTER TABLE habit DROP type');
     }
 }
--- Projet_Ymmersion/migrations/Version20250219191706.php
+++ Projet_Ymmersion/migrations/Version20250219191706.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250219071400 extends AbstractMigration
+final class Version20250219191706 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20250219071400 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user ADD group_name VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE habit DROP type');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user DROP group_name');
+        $this->addSql('ALTER TABLE habit ADD type VARCHAR(10) NOT NULL');
     }
 }
