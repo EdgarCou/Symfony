955027d1b39b38fa5bd0f56d5fb7fba57dc9574b
Ronan <ronan.dupas@ynov.com>
2025-02-21 02:07:17 +0100

    add score

--- Projet_Ymmersion/migrations/Version20250221003546.php
+++ Projet_Ymmersion/migrations/Version20250221003546.php
@@ -10,11 +10,15 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
+<<<<<<< HEAD
 <<<<<<<< HEAD:Projet_Ymmersion/migrations/Version20250221003546.php
 final class Version20250221003546 extends AbstractMigration
 ========
 final class Version20250219190412 extends AbstractMigration
 >>>>>>>> 3691f42 (make group and join them, habit for group):Projet_Ymmersion/migrations/Version20250219190412.php
+=======
+final class Version20250221003546 extends AbstractMigration
+>>>>>>> 70334dd (add score)
 {
     public function getDescription(): string
     {
@@ -24,20 +28,28 @@ final class Version20250219190412 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+<<<<<<< HEAD
 <<<<<<<< HEAD:Projet_Ymmersion/migrations/Version20250221003546.php
         $this->addSql('ALTER TABLE `group` ADD score INT NOT NULL');
 ========
         $this->addSql('ALTER TABLE habit ADD type VARCHAR(10) NOT NULL');
 >>>>>>>> 3691f42 (make group and join them, habit for group):Projet_Ymmersion/migrations/Version20250219190412.php
+=======
+        $this->addSql('ALTER TABLE `group` ADD score INT NOT NULL');
+>>>>>>> 70334dd (add score)
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+<<<<<<< HEAD
 <<<<<<<< HEAD:Projet_Ymmersion/migrations/Version20250221003546.php
         $this->addSql('ALTER TABLE `group` DROP score');
 ========
         $this->addSql('ALTER TABLE habit DROP type');
 >>>>>>>> 3691f42 (make group and join them, habit for group):Projet_Ymmersion/migrations/Version20250219190412.php
+=======
+        $this->addSql('ALTER TABLE `group` DROP score');
+>>>>>>> 70334dd (add score)
     }
 }
